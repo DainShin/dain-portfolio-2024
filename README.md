83cadae3d764e5b6ef02c0511f9696a90e0c504c
DainShin <di.shin0611@gmail.com>
2024-08-27 06:02:13 +0900

    newly created repository

---
+++ README.md
@@ -0,0 +1,28 @@
+# Description
+This is a portfolio website composed of 4 pages. 
+The code are based on Bootstrap, Javascript and jQuery.
+
+# Live site
+https://comp2068jsframeworks-igt8.onrender.com/
+
+
+# References
+
+### Text Animation
+https://codepen.io/dotonion/pen/MWmMJXz 
+.Onion (@dotonion)
+
+
+### OwlCarousel
+https://codepen.io/khadkamhn/pen/RxVKXP
+MOHAN KHADKA(@khadkamhn)
+
+
+### Circular progressbar
+https://codepen.io/iftikharrasha/pen/mdELVpN
+Iftikhar Rasha (@iftikharrasha)
+
+
+### Skill bar
+https://codepen.io/Beni70/pen/BaNxQyv
+Beni (@Beni70)
---
+++ public/javascripts/script.js
@@ -0,0 +1,199 @@
+
+$(document).ready(function () {
+    const elts = {
+        text1: document.getElementById("text1"),
+        text2: document.getElementById("text2")
+    };
+
+    const texts = [
+        "Hello,",
+        "I'm ",
+        "Dain",
+        ":)",
+    ];
+
+    const morphTime = 1; // duration for changing the text
+    const cooldownTime = 0.25; // wait time between the texts
+
+    let textIndex = texts.length - 1;
+    let time = new Date();
+    let morph = 0;
+    let cooldown = cooldownTime;
+
+    if (elts.text1 && elts.text2) { // Check if elements are found
+        elts.text1.textContent = texts[textIndex % texts.length];
+        elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+        function doMorph() {
+            morph -= cooldown;
+            cooldown = 0;
+
+            let fraction = morph / morphTime;
+
+            if (fraction > 1) {
+                cooldown = cooldownTime;
+                fraction = 1;
+            }
+
+            setMorph(fraction);
+        }
+
+        function setMorph(fraction) {
+            elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+            elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+            fraction = 1 - fraction;
+            elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+            elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+            elts.text1.textContent = texts[textIndex % texts.length];
+            elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+        }
+
+        function doCooldown() {
+            morph = 0;
+
+            elts.text2.style.filter = "";
+            elts.text2.style.opacity = "100%";
+
+            elts.text1.style.filter = "";
+            elts.text1.style.opacity = "0%";
+        }
+
+        function animate() {
+            requestAnimationFrame(animate);
+
+            let newTime = new Date();  // Removed the extra 'S' character here
+            let shouldIncrementIndex = cooldown > 0;
+            let dt = (newTime - time) / 1000;
+            time = newTime;
+
+            cooldown -= dt;
+
+            if (cooldown <= 0) {
+                if (shouldIncrementIndex) {
+                    textIndex++;
+                }
+
+                doMorph();
+            } else {
+                doCooldown();
+            }
+        }
+        animate();
+    }
+
+
+  
+});
+
+  // skills: when this function is in the first document.ready function, errors occur, so I put it out 
+  $(document).ready(function () {
+    // Change the color of the selected menu 
+    var currentPage = window.location.pathname;
+
+    // if the current page is the same as the clicked page then add the active class
+    $(".nav-link").each(function () {
+        var linkPage = $(this).attr("href");
+        if (currentPage === linkPage) {
+            $(this).addClass("active");
+        }
+    });
+
+    // add the active class to the clicked menu
+    $(".nav-link").click(function () {
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    // Function to trigger hard skill animation
+    function animateHardSkills() {
+        $(".skill-per").each(function () {
+            var $this = $(this);
+    
+            // Check if the animation has already been run
+            if ($this.data('animated')) {
+                return; // Exit if animation has already run
+            }
+    
+            var per = parseInt($this.attr("per"), 10);  // 퍼센트 값이 문자열이 아닌 정수로 처리되도록 수정
+            $this.css("width", per + "%");
+            $({ animatedValue: 0 }).animate(
+                { animatedValue: per },
+                {
+                    duration: 1000,
+                    step: function (now) {
+                        $this.attr('data-per', Math.floor(now) + "%");
+                    },
+                    complete: function () {
+                        $this.attr('data-per', per + "%");
+                    }
+                }
+            );
+            // Mark this element as animated
+            $this.data('animated', true);
+        });
+    }
+
+     // Function to trigger soft skill animation
+     function animateSoftSkills() {
+        $(".circlechart").each(function () {
+            var $this = $(this);
+            var $circle = $this.find('.circle-chart__circle');
+
+            // Add the animation class when the section is in view
+            $circle.css('animation', 'circle-chart-fill 2s reverse');
+        });
+        
+    }
+
+    // Trigger animations when the scroll reaches the appropriate sections
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+             // Hard skills section animation trigger
+             var skillsTop = $('.skills-container').offset().top - 100;  // Adjust the offset as needed
+             if (scrollTop + windowHeight > skillsTop) {
+                 animateHardSkills();
+             }
+    
+        // Soft skills section animation trigger
+        var softSkillsTop = $('.skills-area').offset().top + 200;  // Adjust the offset as needed
+        if (scrollTop + windowHeight > softSkillsTop) {
+            animateSoftSkills();
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    function checkVisibility() {
+        const windowHeight = $(window).height();
+        const scrollTop = $(window).scrollTop();
+
+        $('.experience.hidden').each(function () {
+            const $this = $(this);
+            const offsetTop = $this.offset().top + 100;
+
+            console.log(offsetTop);
+
+            console.log(`Element offsetTop: ${offsetTop}`);
+            console.log(`Scroll position: ${scrollTop + windowHeight}`);
+
+            // 요소가 화면에 보이는지 확인하는 조건을 정확하게 설정합니다.
+            if (scrollTop + windowHeight > offsetTop) {
+                console.log('Element is becoming visible:', $this);
+                $this.removeClass('hidden').addClass('visible');
+            }
+        });
+    }
+
+    // 초기 호출
+    checkVisibility();
+
+    // 스크롤 이벤트 핸들러
+    $(window).on('scroll', function () {
+        requestAnimationFrame(checkVisibility);
+    });
+});
